618c9d5d9d8241c4de75c43e8ec2aedbe1d51d3c
amyyluna <afrancis@beaconschool.org>
2019-03-02 17:26:15 +0000

    1234

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,1 +1,9 @@
 # file name: problemSetDay4.py
+strength = 5
+while strength <= 10:
+    print(strength)
+    strength += 1
+    if strength < 10:
+        print('Your strength level is low :(')
+    elif strength == 10:
+        print('Great Job! Your strength level is at it maximum level :)')
